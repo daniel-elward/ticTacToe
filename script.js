68978e72392402bacc8231f041a8eb5d6e30f9fe
LR18 <daniel.elward@hotmail.com>
2024-10-31 21:05:42 -0300

    basic game logic added
    first pass at game logic, players turns are alternated.
    gameBoard changed to 2d array instead of object.
    
    win condition needs to be added.

--- script.js
+++ script.js
@@ -1,85 +1,114 @@
-function gameBoard() {
-    let gameGrid = {
-        a1: null, b1: null, c1: null,
-        a2: null, b2: null, c2: null,
-        a3: null, b3: null, c3: null   
-    };
+// IIFE example
+const gameBoard = (function gameBoard() {
 
-    return gameGrid;
-};
+    const game = [
+        [null, null, null],
+        [null, null, null],
+        [null, null, null],
+    ]
+    return game;
+}) ();
 
 //this works to create user, do I need to improve on this??
-function createUser(symbol) {
-    const userName = prompt("Please enter your name");
+function createUser(symbol, turnID) {
+    const name = prompt("Please enter your name");
 
-    return {userName, symbol, score: 0};
+    return {name, symbol, turnID, score: 0};
 };
 
-/*
-
-OLD CODDE -----
-
-function createPlayer(playerName, symbol){
-
-    return {
-        playerName = prompt("Player, enter your name");
-    }
-    /*playerOne = prompt("Player 1, enter your name");
-    playerTwo = prompt("Player 2, enter your name");
-    
-    players = {{PlayerOne: playerOne, symbol: "X", score: 0} 
-               {PlayerTwo: playerTwo, symbol: "O", score: 0}}
-
-    return players;
-}
-
-// gets players names, stored as obj.
-//const playerNames = createPlayer();
-*/
-
+// runs the game, too much repeated code? specifically in playTurn()
 function playGame(){
-    
-    let gameDisplay = gameBoard();
-    console.log(gameDisplay);
 
-    const playerOne = createUser("X");
-    const playerTwo = createUser("O");
+    //generate players
+    const playerOne = createUser("X", 1);
+    const playerTwo = createUser("O", 2);
 
-    console.log(playerOne);
-    console.log(playerTwo);
+    //logs for testing
+    console.log(playerOne)
+    console.log(playerTwo)
 
-        function makeMove() {
-            for(let i = 1; i <= 2; i++){
+    // 1 = playerOne 2 = playerTwo
+    let turnCounter = 1;
 
-                if (i % 2 != 0) {
-                    alert(`${playerOne.userName} it is your move`);
+    function playTurn() {
+        if (turnCounter === 1) {
 
-                    let playerMove = prompt(`${playerOne.userName} type your move e.g, a1, b1 etc`);
+            (function formatPlayerChoice() {
+                playerSelection = prompt(`${playerOne.name} please make your selection \n example, [1 to 3] for row, [1 to 3] for column. '12' would be first position on row 1, second position column 2.`);
 
-                    gameDisplay[playerMove] = playerOne.symbol;
+                //split user input into an array
+                selectionArray = playerSelection.split(""); 
 
-                    //log tests
-                    console.log(gameDisplay)
-                    console.log(`Players move choice: ${playerMove}`)
-                    console.log(`Players symbol: ${playerOne.symbol}`)
-                } else {
-                    alert(`${playerTwo.userName} it is your move`);
+                //decrement by 1 to account for array indexes
+                for (let i = 0; i < 2; i++){
+                    selectionArray[i] -= 1;
+                }
+                return selectionArray;
+            })();
 
-                    playerMove = prompt(`${playerTwo.userName} type your move e.g, a1, b1 etc`);
+            let usersSelectionIndexed = gameBoard[selectionArray[0]][selectionArray[1]];
 
-                    gameDisplay[playerMove] = playerTwo.symbol;
+            if (usersSelectionIndexed === null){
 
-                    //log tests
-                    console.log(gameDisplay)
-                    console.log(`Players move choice: ${playerMove}`)
-                    console.log(`Players symbol: ${playerTwo.symbol}`)
-                } 
+                gameBoard[selectionArray[0]][selectionArray[1]] = playerOne.symbol;
+                turnCounter = 2;
+
+                //display the game state
+                console.log("game board display:");
+                console.log(gameBoard);
+            } else {
+                alert("ERROR!, try again");
+                playTurn()
             }
-        };
 
-        makeMove();
+        } else if (turnCounter === 2) {
+
+            (function formatPlayerChoice() {
+                playerSelection = prompt(`${playerTwo.name} please make your selection \n example, [1 to 3] for row, [1 to 3] for column. '12' would be first position on row 1, second position column 2.`);
+
+                //split user input into an array
+                selectionArray = playerSelection.split(""); 
+
+                //decrement by 1 to account for array indexes
+                for (let i = 0; i < 2; i++){
+                    selectionArray[i] -= 1;
+                }
+                return selectionArray;
+            })();
+
+            //console.log(selectionArray)
+            //console.log(gameBoard[1][0])
+
+            let usersSelectionIndexed = gameBoard[selectionArray[0]][selectionArray[1]];
+
+            if (usersSelectionIndexed === null){
+
+                gameBoard[selectionArray[0]][selectionArray[1]] = playerTwo.symbol;
+                turnCounter = 1;
+
+                //display the game state
+                console.log("game board display:");
+                console.log(gameBoard);
+            } else {
+                alert("ERROR!, try again");
+                playTurn()
+            }
+        }
+    } 
+
+
+    //checks for a win condition
+    function winCondition(){
+    }
+
+    for (let i = 0; i < 2; i++){
+        
+        playTurn();
+        //winCondition();
+        //console.log(x) 
+    }
+
+
 };
 
 playGame();
-
-//console.log(players)
