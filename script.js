17d2b720680c0b8223c3cc4b2b751f9788050c53
LR18 <daniel.elward@hotmail.com>
2024-11-01 10:20:54 -0300

    console version complete
    fully functional console version completed.
    
    Some areas need reworking 'DRY'.
    
    GUI implementation to follow.

--- script.js
+++ script.js
@@ -2,9 +2,9 @@
 const gameBoard = (function gameBoard() {
 
     const game = [
-        [null, null, null],
-        [null, null, null],
-        [null, null, null],
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
     ]
     return game;
 }) ();
@@ -48,7 +48,9 @@ function playGame(){
 
             let usersSelectionIndexed = gameBoard[selectionArray[0]][selectionArray[1]];
 
-            if (usersSelectionIndexed === null){
+            //let isString = typeof gameBoard
+
+            if (typeof usersSelectionIndexed != "string"){
 
                 gameBoard[selectionArray[0]][selectionArray[1]] = playerOne.symbol;
                 turnCounter = 2;
@@ -81,7 +83,7 @@ function playGame(){
 
             let usersSelectionIndexed = gameBoard[selectionArray[0]][selectionArray[1]];
 
-            if (usersSelectionIndexed === null){
+            if (typeof usersSelectionIndexed != "string"){
 
                 gameBoard[selectionArray[0]][selectionArray[1]] = playerTwo.symbol;
                 turnCounter = 1;
@@ -99,16 +101,141 @@ function playGame(){
 
     //checks for a win condition
     function winCondition(){
+        /*
+        index goes row then column
+        THIS IS THE ACTUAL INDEXES
+        [0,0, 0,1, 0,2],
+        [1,0, 1,1, 1,2],
+        [2,0, 2,1, 2,2],
+
+        THIS IS +1 FOR HUMAN ENTRY
+        [1,1, 1,2, 1,3],
+        [2,1, 2,2, 2,3],
+        [3,1, 3,2, 3,3],
+        */
+
+        //win condition code can't be optimal? look into ways to rewrite this later
+
+        //check left column 
+        if (gameBoard[0][0] === playerOne.symbol && gameBoard[1][0] === playerOne.symbol && gameBoard[1][0] === playerOne.symbol && gameBoard[2][0] === playerOne.symbol) {
+
+            console.log(`${playerOne.name} wins - left column`);
+            playerOne.score++;
+
+            winner = playerOne.name
+            return winner;
+
+        } else if (gameBoard[0][0] === playerTwo.symbol && gameBoard[1][0] === playerTwo.symbol && gameBoard[1][0] === playerTwo.symbol && gameBoard[2][0] === playerTwo.symbol){
+            console.log(`${playerTwo.name} wins - left column`)
+            playerTwo.score++
+
+            winner = playerTwo.name
+            return winner;
+            
+        }
+
+        //check right column 
+        if (gameBoard[0][2] === playerOne.symbol && gameBoard[1][2] === playerOne.symbol && gameBoard[1][2] === playerOne.symbol && gameBoard[2][2] === playerOne.symbol) {
+            
+            console.log(`${playerOne.name} wins - right column`)
+            playerOne.score++
+
+            winner = playerOne.name
+            return winner;
+
+        } else if (gameBoard[0][2] === playerTwo.symbol && gameBoard[1][2] === playerTwo.symbol && gameBoard[1][2] === playerTwo.symbol && gameBoard[2][2] === playerTwo.symbol) {
+            console.log(`${playerTwo.name} wins - right column`)
+            playerTwo.score++
+
+            winner = playerTwo.name
+            return winner;
+            
+        }
+
+        //check top row 
+        if (gameBoard[0][0] === playerOne.symbol && gameBoard[0][1] === playerOne.symbol && gameBoard[0][1] === playerOne.symbol && gameBoard[0][2] === playerOne.symbol) {
+            console.log(`${playerOne.name} wins - top row`)
+            playerOne.score++
+
+            winner = playerOne.name
+            return winner;
+            
+        } else if (gameBoard[0][0] === playerTwo.symbol && gameBoard[0][1] === playerTwo.symbol && gameBoard[0][1] === playerTwo.symbol && gameBoard[0][2] === playerTwo.symbol) {
+            console.log(`${playerTwo.name} wins - top row`)
+            playerTwo.score++
+
+            winner = playerTwo.name
+            return winner;
+            
+        }
+
+        //check middle row 
+        if (gameBoard[1][0] === playerOne.symbol && gameBoard[1][1] === playerOne.symbol && gameBoard[1][1] === playerOne.symbol && gameBoard[1][2] === playerOne.symbol) {
+            console.log(`${playerOne.name} wins - middle row`)
+            playerOne.score++
+
+            winner = playerOne.name
+            return winner;
+            
+        } else if (gameBoard[1][0] === playerTwo.symbol && gameBoard[1][1] === playerTwo.symbol && gameBoard[1][1] === playerTwo.symbol && gameBoard[1][2] === playerTwo.symbol) {
+            console.log(`${playerTwo.name} wins - middle row`)
+            playerOne.score++
+
+            winner = playerTwo.name
+            return winner;
+            
+        }
+
+        //check bottom row
+        if (gameBoard[2][0] === playerOne.symbol && gameBoard[2][1] === playerOne.symbol && gameBoard[2][1] === playerOne.symbol && gameBoard[2][2] === playerOne.symbol) {
+            console.log(`${playerOne.name} wins - bottom row`)
+            playerOne.score++
+
+            winner = playerOne.name
+            return winner;
+            
+        } else if (gameBoard[2][0] === playerTwo.symbol && gameBoard[2][1] === playerTwo.symbol && gameBoard[2][1] === playerTwo.symbol && gameBoard[2][2] === playerTwo.symbol) {
+            console.log(`${playerTwo.name} wins - bottom row`)
+            playerTwo.score++
+
+            winner = playerTwo.name
+            return winner;
+            
+        }
+
+        //check middle and corners
+        if (gameBoard[1][1] === playerOne.symbol && gameBoard[0][2] === playerOne.symbol && gameBoard[1][1] === playerOne.symbol && gameBoard[2][0] === playerOne.symbol ||
+            gameBoard[1][1] === playerOne.symbol && gameBoard[0][0] === playerOne.symbol && gameBoard[1][1] === playerOne.symbol && gameBoard[2][2] === playerOne.symbol) {
+                console.log(`${playerOne.name} wins - diagonal`)
+                playerOne.score++
+
+            winner = playerOne.name
+            return winner;
+            
+        } else if (gameBoard[1][1] === playerTwo.symbol && gameBoard[0][2] === playerTwo.symbol && gameBoard[1][1] === playerTwo.symbol && gameBoard[2][0] === playerTwo.symbol ||
+            gameBoard[1][1] === playerTwo.symbol && gameBoard[0][0] === playerTwo.symbol && gameBoard[1][1] === playerTwo.symbol && gameBoard[2][2] === playerTwo.symbol) {
+                console.log(`${playerTwo.name} wins - diagonal`)
+                playerTwo.score++
+
+            winner = playerTwo.name
+            return winner;
+            
+            }
+
+            return winner = "none";
     }
 
-    for (let i = 0; i < 2; i++){
-        
-        playTurn();
-        //winCondition();
-        //console.log(x) 
+for (let i = 0; i < 9; i++){
+
+    playTurn();
+    winCondition();
+
+    if (winner === playerOne.name || winner === playerTwo.name){
+        console.log(`${playerOne.name} score is ${playerOne.score}`)
+        console.log(`${playerTwo.name} score is ${playerTwo.score}`)
+        break;
     }
-
-
+}
 };
 
 playGame();
