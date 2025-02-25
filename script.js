5089043951fe17463e173c3eaaf27aca330f6de5
LR18 <daniel.elward@hotmail.com>
2025-02-25 18:57:49 -0400

    player and game objects added

---
+++ script.js
@@ -0,0 +1,21 @@
+// game object
+const gameController = {
+    gameBoard: [null, null, null, null, null, null, null, null, null]
+}
+
+//player object 
+const playerController = {
+
+    playerOne: {
+        name: "player one",
+        symbool: "X",
+        score: 0
+    },
+
+    playerTwo: {
+        name: "player two",
+        symbol: "O",
+        score: 0
+    }
+}
+
