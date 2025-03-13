cd47c9df91d34637618a5d1c75eeb95a2d5cbaf6
LR18 <daniel.elward@hotmail.com>
2025-03-13 01:18:25 -0300

    move validation logic
    started working on validating player choices

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // game object
 const gameController = {
-    gameBoard: [null, null, null, null, null, null, null, null, null]
+    gameboard: [null, null, null, null, null, null, null, null, null]
 }
 
 //player object 
@@ -8,7 +8,7 @@ const playerController = {
 
     playerOne: {
         name: "player one",
-        symbool: "X",
+        symbol: "X",
         score: 0
     },
 
@@ -18,4 +18,48 @@ const playerController = {
         score: 0
     }
 }
+
+function gameFlow() {
+
+    let playerSelection = null;
+
+    function getPlayerSelection() {
+
+        playerSelection = prompt(`${currentPlayer.name} is next, make your selection between 1 and 9 \n 1 | 2 | 3 \n 4 | 5 | 6 \n 7 | 8 | 9`);
+
+        if (playerSelection < 1 || playerSelection > 9) {
+            
+            alert("ERROR! Invalid selection please try again");
+            getPlayerSelection();
+        };
+
+        //minus 1 because of arraying indexes
+        playerSelection = playerSelection - 1;
+
+        if (!gameController.gameboard[playerSelection] === null) {
+            
+            alert("ERROR! That position has already been played");
+            getPlayerSelection();
+        };
+
+        return playerSelection;
+    };
+
+    let currentPlayer = playerController.playerOne;
+
+    for (let i = 0; i < 3; i++) {
+
+        getPlayerSelection();
+
+        //update (and log) array with player selection .. need logic to validate player choice
+        gameController.gameboard[playerSelection] = currentPlayer.symbol;
+    
+        console.log(gameController.gameboard)
+        
+        //switch current player
+        const playerSwitch = (currentPlayer === playerController.playerOne) ? currentPlayer = playerController.playerTwo : currentPlayer = playerController.playerOne;
+    }
+}
+
+gameFlow()
 
