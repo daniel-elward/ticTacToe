b7a4d2aa7ba4481457ea7c9f6526baaa5dfcdf20
Daniel Elward <daniel.elward@hotmail.com>
2025-09-12 13:15:28 -0300

    completed console game GUI next

--- script.js
+++ script.js
@@ -11,57 +11,111 @@ const playerController = {
     }
 };
 
-const PlayerOne = playerController.createPlayer('Dan', 'X');
-const PlayerTwo = playerController.createPlayer('Nad', 'O');
+const playerOne = playerController.createPlayer('Dan', 'X');
+const playerTwo = playerController.createPlayer('Nad', 'O');
 
 //gameboard object
-const gameboard = {
+const game = {
 
-    activeGame:['-','-','-','-','-','-','-','-','-']
+    activeGame:[' ',' ',' ',' ',' ',' ',' ',' ',' '],
 };
 
-//gameflow object
+//gameflow
 (function gameFlow() {
 
     let gameWon = false;
-    let activePlayer = PlayerOne;
-    let choice = 1;
+    let activePlayer = playerOne;
+    let choice = 1; //placeholder
 
     function switchPlayer() {
 
-        if(activePlayer === PlayerOne) {
+        if(activePlayer === playerOne) {
 
-            activePlayer = PlayerTwo;
+            activePlayer = playerTwo;
         } else {
 
-            activePlayer = PlayerOne;
+            activePlayer = playerOne;
         };
     };
 
     //store players move
     function playerChoice() {
 
-        choice = prompt(`${activePlayer.name}'s turn 1-9`);
+        choice = prompt(`${activePlayer.name}'s turn 1-9 \n
+                         ${game.activeGame[0]} | ${game.activeGame[1]} | ${game.activeGame[2]} \n
+                         ${game.activeGame[3]} | ${game.activeGame[4]} | ${game.activeGame[5]} \n
+                         ${game.activeGame[6]} | ${game.activeGame[7]} | ${game.activeGame[8]}`);
+
+        if (game.activeGame[choice - 1] != ' ') {
+
+            alert(`ERROR, please try again \n 
+                ${game.activeGame[0]} | ${game.activeGame[1]} | ${game.activeGame[2]} \n
+                ${game.activeGame[3]} | ${game.activeGame[4]} | ${game.activeGame[5]} \n
+                ${game.activeGame[6]} | ${game.activeGame[7]} | ${game.activeGame[8]}`);
+            playerChoice();
+        };
     };
 
     //update gameboard with players choice
     function updateBoard() {
 
-        gameboard.activeGame[choice - 1] = activePlayer.symbol;
+        game.activeGame[choice - 1] = activePlayer.symbol; // - to account for array indexing
     };
 
-    for (let i = 0; i < 3; i++) {
+    function checkWin() {
 
-        console.log(` Gameboard START of loop ${gameboard.activeGame}`);
+        //after spending multiple hours trying to work through this
+        //I used a youtube tutorial for help
+        //credit: Bro Code.
+        winState = [[0,1,2],
+                    [3,4,5],
+                    [6,7,8],
+
+                    [0,3,6],
+                    [1,4,7],
+                    [2,5,8],
+
+                    [0,4,8],
+                    [2,4,6]];
+
+        for (let i = 0; i < winState.length; i++) {
+
+            const condition = winState[i];
+            const cellA = game.activeGame[condition[0]];
+            const cellB = game.activeGame[condition[1]];
+            const cellC = game.activeGame[condition[2]];
+
+            if (cellA === ' ' || cellB === ' ' || cellC === ' '){
+
+                continue;
+            };
+
+            if (cellA === cellB && cellB === cellC) {
+
+                gameWon = true;
+                alert(`GAME WON \n
+                    ${game.activeGame[0]} | ${game.activeGame[1]} | ${game.activeGame[2]} \n
+                    ${game.activeGame[3]} | ${game.activeGame[4]} | ${game.activeGame[5]} \n
+                    ${game.activeGame[6]} | ${game.activeGame[7]} | ${game.activeGame[8]}`)
+                break;
+            };
+        };
+    };
+
+    for (let i = 0; i < 9; i++) {
+
+        if (gameWon === true) { break; };
 
         playerChoice();
-        console.log(`${activePlayer.name} chose ${choice}`);
 
         updateBoard();
 
         switchPlayer();
-        console.log(` Gameboard END of loop ${gameboard.activeGame}`);
+
+        checkWin();
+
+        console.log(`gameWon = ${gameWon}`)
+
+        console.log(` Gameboard END of loop ${game.activeGame}`);
     };
-    
-    //prompt(`${PlayerOne.name}, it is your turn. 1-9`)
 })();
